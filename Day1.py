b67fd3d00f6a0042aaa5a171fcf71e0c563254b5
JasperChiou <43606464+JasperChiou@users.noreply.github.com>
2019-02-21 15:03:32 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+print('hello,world!')
+
+
+# In[2]:
+
+
+print ("Hello")
+
+
+# In[3]:
+
+
+print('Hello','world')
+
+
+# In[4]:
+
+
+print('hello', 'world', sep=', ', end='!')
+
+
+# In[5]:
+
+
+print('YooKeyo','Sangranghae',sep=',',end='!\n')
+
+
+# In[6]:
+
+
+print('YooKeyo','Sangranghae',sep=',',end='!\n')
+print('Jinjiaya!')
+
